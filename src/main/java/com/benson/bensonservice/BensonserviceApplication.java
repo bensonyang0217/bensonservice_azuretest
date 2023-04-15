29e63eee0c608aa481f382720c414ced80605465
Benson Yang <f932037@gmail.com>
2023-04-15 20:23:21 +0800

    init

---
+++ src/main/java/com/benson/bensonservice/BensonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.benson.bensonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BensonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BensonserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benson/bensonservice/BensonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benson.bensonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BensonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
